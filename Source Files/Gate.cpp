ae9f2bf7cb96b4d2d4fcba900bf9ddcb6523592f
markst47 <markstmichell@cedarville.edu>
2026-03-31 13:32:13 -0400

    Gate.h and .cpp changes

--- Source Files/Gate.cpp
+++ Source Files/Gate.cpp
@@ -7,22 +7,21 @@ Purpose: This is the implementation for the gate class.
 
 #include "Gate.h"
 
-Gate::Gate(type, int delay, Wire *in1, Wire *in2, Wire *out) {
-
-}
+Gate::Gate(type gateType, int delay, Wire *input1, Wire *input2, Wire *output):
+    gateType(gateType), delay(delay), in1(input1), in2(input2), out(output) {}
 
 int Gate::getDelay() const {
+    return delay;
+}
+
+Wire* Gate::getInput(int wireNum) const {
 
 }
 
-Wire* Gate::getInput(int) const {
+Wire* Gate::getOutput(int wireNum) const {
 
 }
 
-Wire* Gate::getOutput(int) const {
-
-}
-
-Wire::state Gate::evaluate(type, int delay, Wire *in1, Wire *in2, Wire *out) {
+Wire::curState Gate::evaluate(type, int delay, Wire *in1, Wire *in2, Wire *out) {
 
 }
